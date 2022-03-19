1f525d4b8af270f7c5a20ecdd8d673c7d27b72b5
Victor Novokshenov <victornov@bk.ru>
2022-03-19 20:52:08 +0500

    Initial commit

---
+++ js/tabs.js
@@ -0,0 +1,37 @@
+const tabButtons = document.querySelectorAll('.design-list__item');
+const tabDescriptions = document.querySelectorAll('.design__descr');
+const tabImages = document.querySelectorAll('.design-images');
+const designBlockImages = document.querySelectorAll('.design-block__img');
+const tabHeaders = document.querySelectorAll('.design__title')
+
+const changeContent = (array, value) => {
+    array.forEach((item) => {
+        item.dataset.tabsField === value ?
+            item.classList.remove('hidden') : item.classList.add('hidden');
+    });
+}
+
+tabButtons.forEach((tabButton) => {
+    tabButton.addEventListener('click', (e) => {
+        const dataValue = tabButton.dataset.tabsHandler;
+
+        changeContent(tabDescriptions, dataValue);
+        changeContent(tabImages, dataValue);
+        changeContent(tabHeaders, dataValue);
+        changeContent(designBlockImages, dataValue);
+
+        tabHeaders.forEach((header) => {
+            if (header.dataset.tabsField === dataValue) {
+                document.title = header.innerText;
+            }
+        })
+
+        tabButtons.forEach((btn) => {
+            if (btn === e.target) {
+                btn.classList.add('design-list__item_active');
+            } else {
+                btn.classList.remove('design-list__item_active');
+            }
+        });
+    });
+});
